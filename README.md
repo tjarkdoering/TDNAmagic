5494ed36668e9c3cf33f2eb3b77fb63476303290
Tjark <github@tjark-doering.de>
2021-04-27 16:08:40 +0200

    Added the script (and more info to the README)

--- README.md
+++ README.md
@@ -5,8 +5,14 @@ Shell script to complement TDNAscan in some minor way. To be more precise, it al
 ## Usage
 
 1. Make sure your TDNAscan is working. Invoke `python tdnascan.py --version` from the TDNAscan directory or something.
-2. Move the script to any of your `bin` path and make it executable. Preferably of any of the user accessible ones.
-3. Navigate to your working directory (containing your the two paired read files, the T-DNA sequence file and the genome sequence file).
+2. Move the script to any of your `bin` path (so you can invoke it from anywhere) and make it executable. Preferably of any of the user accessible ones.
+3. Open the script in an editor of your choice and add the path to your TDNAscan dir and check the contents that it doesn't do anything malicious.
+4. Navigate to your working directory (containing your the two paired read files, the T-DNA sequence file and the genome sequence file).
 4. Call the script:
 
-  $ tdnamagic.sh -1 forward.fq -2 reverse.fq -t t-dna.fa -g ref_genome.fa
+    $ tdnamagic.sh -1 forward.fq -2 reverse.fq -t t-dna.fa -g ref_genome.fa
+
+
+## Troubles using this?
+
+Feel free to contact me or any of your resident bioinformatician :wink:
---
+++ tdnamagic.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Author : Tjark Döring (https://github.com/tjarkdoering)
+# Purpose : To remove unnecessary file moving from the workflow of using TDNAscan.
+# How does it work : From your working directory, the called for files are copied to TDNAscan directory. From there the program is executed and the output is copied back to your working directory. After some cleanup, everything should be good. It's like... magic...
+# Disclaimer : Use at your own risk.
+
+# Usage : Similar to TDNAscan (to avoid confusion), but you will have to not add the -p argument. Example below:
+#  sh tdnamagic.sh -1 forward.fq -2 reverse.fq -t t-dna.fa -g ref_genome.fa
+# (the sh can usually be omitted)
+
+
+# Enter your Path to the TDNAscan below
+path_tdnascan=/path/to/your/TDNAscan/
+
+
+# Storing the working dir for later use
+path_projectdir=$(pwd)
+
+
+# Communicate stuff to the user
+echo "Hi $USER, thanks for using TDNAmagic, good luck with your work!"
+echo "You have given me $# arguments. Good so far."
+
+echo
+echo "TDNAscan dir is:"
+echo $path_tdnascan
+
+echo
+echo "and you are working in:"
+echo $path_projectdir
+
+
+# Change into TDNAscan dir
+cd $path_tdnascan
+
+
+# Invoke tdnascan with arguments given by user
+# python tdnascan.py --version
+# python tdnascan.py -1 $2 -2 $4 -t $6 -g $8 -t TDNAoutput
+
+
+# Move the output dir to working dir
+mv TDNAoutput $path_projectdir
+
+cd $path_projectdir
+
+echo
+echo "Something happened. Check you files. Bye."
+
